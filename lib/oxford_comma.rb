0404aa05157f2452dee71673eee9539af0afaf1e
decent-scraper-1450 <jaimealba1574@gmail.com>
2020-04-15 17:17:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,8 @@
 def oxford_comma(array)
-iff array.lenght==2
-  return "#{array[1]" and #{array[2]""
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
+  end
+  array.join(", ")    
 end
-array.join
